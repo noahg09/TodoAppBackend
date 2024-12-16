aa45c8c6f60b210f208421743d15c09a8e296795
noahg09 <noah.gruenig@hispeed.ch>
2024-12-16 16:30:02 +0100

    added getter and setter

--- src/main/java/bbw/ng/cypresstutorialbackend/model/Todo.java
+++ src/main/java/bbw/ng/cypresstutorialbackend/model/Todo.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +13,36 @@ public class Todo {
     private String title;
     private boolean completed;
     private String priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
 }
